83b0778cf7a3ddd41a076f4b5bd58c88fb2273de
Virgile Theze <virgile.th@hotmail.fr>
2021-12-13 11:02:15 +0100

    Features: Ajout des fonctionalités de comptage de score, de sauvegarde de score et de passage d'un joueur à l'autre

--- Script.js
+++ Script.js
@@ -2,40 +2,53 @@ const newGame = document.getElementById("NewGame");
 const throwDice = document.getElementById("ThrowDice");
 const hold = document.getElementById("Hold");
 
-const p1Score = document.getElementById("Player1Score");
-const p2Score = document.getElementById("Player2Score");
+var p1Round = document.getElementById("Player1Round");
+var p2Round = document.getElementById("Player2Round");
+var p1Score = document.getElementById("Player1Score");
+var p2Score = document.getElementById("Player2Score");
 
-let p1Round = 0;
-let p2Round = 0;
-let p1Global = 0;
-let p2Global = 0;
-let activePlayer = 0;
+var p1Global = 0;
+var p2Global = 0;
+var activePlayer = 0;
 
 throwDice.addEventListener('click', () => {
    let dice =(Math.floor(Math.random()*6)+1);
+   console.log(dice);
    if (dice == 1) {
     if (activePlayer == 0) {
-        p1Round = 0;
+        p1Round.innerHTML = 0;
         activePlayer = 1;
+    }
+    else {
+        p2Round.innerHTML = 0;
+        activePlayer = 0;
     }
    }
    else {
-        p2Round = 0;
-        activePlayer = 0;
+       if (activePlayer == 0) {
+        p1Round.innerHTML = parseInt(p1Round.innerHTML,10) + dice;
+        
+       }
+       else {
+        p2Round.innerHTML = parseInt(p2Round.innerHTML,10) + dice;
+
+       }
    }
 })
 
 hold.addEventListener('click', () => {
-        alert('Vous avez cliqué sur Hold.');
+        alert("C'est au tour du joueur "+(activePlayer+1)+"." );
         if (activePlayer == 0){
-            p1Score.innerHTML = p1Global + p1Round;
-            p1Global = p1Global + p1Round;
+            p1Score.innerHTML = p1Global + parseInt(p1Round.innerHTML,10);
+            p1Global = p1Global + parseInt(p1Round.innerHTML,10);
+            p1Round.innerHTML = 0;
             activePlayer = 1;
             return;
         }
         else {
-            p2Score.innerHTML = p2Global + p2Round;
-            p2Global = p2Global + p2Round;
+            p2Score.innerHTML = p2Global + parseInt(p2Round.innerHTML,10);
+            p2Global = p2Global + parseInt(p2Round.innerHTML,10);
+            p2Round.innerHTML = 0;
             activePlayer = 0;
             return;
         }
